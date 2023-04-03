a08a50444683ce4f73675d6b7a16691020c7a926
Jose <joseluis0528@gmail.com>
2023-04-03 16:02:36 -0500

    work in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    ToDo(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function ToDo(toDo) {
+  let li = document.createElement('li')
+  let button = document.createElement('button')
+  button.textContent = "❌"
+  li.textContent = `${toDo} `
+  li.appendChild(button)
+  document.querySelector('#tasks').appendChild(li)
+  button.addEventListener('click', handleDelete)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
