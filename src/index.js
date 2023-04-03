f0af8cdd106c9c78f68f9525ae638bf872302c50
Jose <joseluis0528@gmail.com>
2023-04-03 18:14:22 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let form = document.querySelector('form')
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    ToDo(e.target.new_task_description.value)
-    form.reset()
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let newId = document.querySelector('input');
+    newId.id = 'new_task_description';
+    ToDo(e.target.new_task_description.value);
+    form.reset();
   })
 });
 
 function ToDo(toDo) {
-  let li = document.createElement('li')
-  let button = document.createElement('button')
-  button.textContent = "❌"
-  li.textContent = `${toDo} `
-  li.appendChild(button)
-  document.querySelector('#tasks').appendChild(li)
-  button.addEventListener('click', handleDelete)
+  let li = document.createElement('li');
+  li.textContent = `${toDo} `;
+  document.querySelector('#tasks').appendChild(li);
+
+  let button = document.createElement('button');
+  button.textContent = "❌";
+  li.appendChild(button);
+  button.addEventListener('click', handleDelete);
 }
 
 function handleDelete(e) {
-  e.target.parentNode.remove()
+  e.target.parentNode.remove();
 }
